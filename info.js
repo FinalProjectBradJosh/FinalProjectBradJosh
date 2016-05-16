2230aaa38fc3854859e1599c9f19a4c87f984c09
FinalProjectBradJosh <jgamer4591@aol.com>
2016-05-16 00:09:02 -0700

    Create info.js

---
+++ info.js
@@ -0,0 +1,28 @@
+$("#popup").click(function () {
+    var $this = $(this);
+    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 450, "height": 325,
+   'align-content': 'center',
+    'border': '5px solid #5E8C92',
+    'padding': '0.5px', 
+                                                                     
+                                                                     
+                                                                     
+                                                                    
+    });
+
+    
+    var $title = $("<h1>").text($this.data("title"));
+    $("#video-view").html($title).append($iframe);
+    $iframe.wrap("<div class='class-video'>");
+  
+
+       
+});
+
+$(document).ready(function(){
+    $(".button_1").click(function(){
+        $(".spacer").css({"padding-bottom": "20px","margin-bottom": "20px"});
+        $("ul.featured_services li.service_3").css({"padding-right": "00px"});
+    });
+         
+});
