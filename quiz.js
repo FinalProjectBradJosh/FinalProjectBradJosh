b9b5e23202cb77126e74234aaa54c0ffb1e6fe1c
FinalProjectBradJosh <jgamer4591@aol.com>
2016-05-16 00:44:59 -0700

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,38 +1,52 @@
-function dragStart(event) {
+// array 1,variable 1
+var words = ["Clouds", "Silver Iodide", "10-15%"];
+
+
+
+document.getElementById("dragtarget1").innerHTML = words[0]; //grouping html with js
+
+
+document.getElementById("dragtarget2").innerHTML = words[1];
+
+
+document.getElementById("dragtarget3").innerHTML = words[2];
+
+
+// function 1,1 parameter
+function dragStart(event) { //event capture 1
     event.dataTransfer.setData("Text", event.target.id);
    
 }
-
+//function 2 , 1 parameter
 function allowDrop(event) {
-    event.preventDefault();
+    event.preventDefault(); //event capture 2
 }
 
 
-/*
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData("Text");
-    event.target.appendChild(document.getElementById(data));
-    
-}
-*/
-
+// variable 2
 var answer1 = false;
+// variable 3
 var answer2 = false;
+// variable 4
 var answer3 = false;
 
-
-var numbers = 0;
+// function 3, 1 parameter
 function drop(ev)
 {
   ev.preventDefault();
+// variable 5
         var image =ev.dataTransfer.getData("text");
-  if (ev.target.id == document.getElementById(image).getAttribute('data-div')){ 
+// conditional 1
+  if (ev.target.id == document.getElementById(image).getAttribute('data-div')){
+// conditional 2
       if(ev.target.id == "div1"){answer1=true;} 
+// conditional 3
      else if(ev.target.id == "div2"){answer2=true;} 
+//conditional 4
     else if(ev.target.id == "div3"){answer3=true;} 
   ev.target.appendChild(document.getElementById(image));
   }
+//conditional 5
   else if(ev.target.id == "div1")
   {
     answer1 = false;
@@ -40,15 +54,14 @@ function drop(ev)
     ev.target.appendChild(document.getElementById(image));
   }
   
-  
+// conditional 6
   else if(ev.target.id == "div2")
   {
     answer2 = false;
      document.getElementById("check2").innerHTML = "It would help to re-read the section about what they shoot into the clouds.";
     ev.target.appendChild(document.getElementById(image));
   }
-  
-  
+// conditional 7
   else if(ev.target.id == "div3")
   {
     answer3= false;
@@ -58,15 +71,14 @@ function drop(ev)
   
 }
 
-
-$this = document.getElementById("submit");
-
+// function 4
 function video()
 {
   console.log("hello");
   console.log(answer1);
   console.log(answer2);
   console.log(answer3);
+// conditional 8
   if(answer1===true&answer2===true&answer3===true){
      
   }
@@ -75,23 +87,27 @@ function video()
 
 
 
-
-$(".popup").click(function () {
+//function 5
+$(".popup").click(function () {//event capture 3
+//variable 6
     var $this = $(this);
   if(answer1===true&answer2===true&answer3===true){
-    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 400, "height": 300,'align-content': 'center', 'margin':'auto'});
+    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 400, "height": 300});
+//variable 7
     var $title = $("<h1>").text($this.data("title"));
     $("#video-view").html($title).append($iframe);
     $iframe.wrap("<div class='class-video'>");}
 });
 
-
+// function 6
 function init() {
 	'use strict';
     console.log("init()");
-	document.getElementById('theForm').onsubmit = video;
+	document.getElementById('theForm').onsubmit = video;//event capture 4
 }
+
+
 
 // End of init() function.
 console.log("page is loaded.");
-window.onload = init;
+window.onload = init; //event capture 5
