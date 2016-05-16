252490a6dd0b8229b14da2082cef8899dd70d27f
FinalProjectBradJosh <jgamer4591@aol.com>
2016-05-15 17:45:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 $("#popup").click(function () {
     var $this = $(this);
-    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 400, "height": 300});
+    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 500, "height": 400,
+   'position': 'absolute',
+    'right': '0px',
+    'border': '3px solid #5E8C92',
+    'padding': '10px',                                      
+    });
+
+    
     var $title = $("<h1>").text($this.data("title"));
     $("#video-view").html($title).append($iframe);
     $iframe.wrap("<div class='class-video'>");
+  
+
+       
+});
+
+$(document).ready(function(){
+    $(".button_1").click(function(){
+        $(".spacer").css({"padding-top": "260px"});
+    });
+         
 });
