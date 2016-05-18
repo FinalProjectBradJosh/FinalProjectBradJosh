1b0787e620fad5c7cc40443d85f92af489de36f3
FinalProjectBradJosh <jgamer4591@aol.com>
2016-05-17 21:56:14 -0700

    Update info.js

--- info.js
+++ info.js
@@ -1,6 +1,6 @@
 $("#popup").click(function () {
-    var $this = $(this);
-    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 450, "height": 325,
+    var $this = $(this); //var1
+    var $iframe = $("<iframe>").attr("src", /*var2*/ $this.data("link")).css({"width": '100%', "height": '100%',
    'align-content': 'center',
     'border': '5px solid #5E8C92',
     'padding': '0.5px', 
@@ -11,17 +11,17 @@ $("#popup").click(function () {
     });
 
     
-    var $title = $("<h1>").text($this.data("title"));
+    var $title = $("<h1>").text($this.data("title")); //var3
     $("#video-view").html($title).append($iframe);
     $iframe.wrap("<div class='class-video'>");
   
-
+//vars on other files
        
 });
 
 $(document).ready(function(){
     $(".button_1").click(function(){
-        $(".spacer").css({"padding-bottom": "20px","margin-bottom": "20px"});
+        $(".spacer").css({"padding-top": "50px"});
         $("ul.featured_services li.service_3").css({"padding-right": "00px"});
     });
          
